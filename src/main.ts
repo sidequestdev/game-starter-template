39723f089d6d8ad25c1f11adcb7ba6871fa0d0eb
Jake Klassen <jklassendev@gmail.com>
2022-02-05 21:35:39 -0500

    bump canvas for some font

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ if (canvas === null) {
   throw new Error("Could not find canvas element");
 }
 
+canvas.style.width = "640px";
+canvas.style.height = "480px";
+
 const context = canvas.getContext("2d");
 
 if (context === null) {
   throw new Error("Could not obtain 2d context");
 }
 
+context.imageSmoothingEnabled = false;
+
 context.fillStyle = "white";
 context.font = "16px sans-serif";
-context.fillText("Let's make a game", canvas.width / 2 - 64, canvas.height / 2);
+context.fillText(
+  "Let's make a game",
+  canvas.width / 2 - 64,
+  canvas.height / 2 + 8,
+);
