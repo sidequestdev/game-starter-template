2d661c456be93735f5b0712f115c34b6949c4bdc
Jake Klassen <jklassendev@gmail.com>
2022-02-05 21:04:02 -0500

    little starter message

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
-const div = document.createElement("div");
-div.innerHTML = "Let's make a game 🎮";
-document.body.appendChild(div);
+const canvas = document.querySelector<HTMLCanvasElement>("canvas");
+if (canvas === null) {
+  throw new Error("Could not find canvas element");
+}
+
+const context = canvas.getContext("2d");
+
+if (context === null) {
+  throw new Error("Could not obtain 2d context");
+}
+
+context.fillStyle = "white";
+context.font = "16px sans-serif";
+context.fillText("Let's make a game", canvas.width / 2 - 64, canvas.height / 2);
